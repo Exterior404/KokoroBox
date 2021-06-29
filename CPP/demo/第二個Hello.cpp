6f8d85ebf74e191471335d12c9b83065977f9a7a
NightFeather <reingeass@gmail.com>
2021-06-29 17:06:49 +0800

    Create 第二個Hello.cpp

---
+++ CPP/demo/第二個Hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int Hand() {
+  cout << "Welcome Hell" << endl;
+
+  return pow(2,3);//2的3次方
+}
+
+int main() {
+cout <<Hand() <<endl;
+
+  std::cout << "Hello World!\n";
+
+  return 0;
+}
