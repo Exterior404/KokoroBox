ad503117dd9586e358110bc60cff75b0a2009a7c
NightFeather <reingeass@gmail.com>
2021-07-02 19:54:20 +0800

    Create NF02.java

---
+++ Java/demo/NF02.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String args[]) {
+		int num;
+		num = 100;
+		System.out.println("This is num = " + num);
+		num = num * 2;
+		System.out.println("num x2 = ");
+		System.out.println(num);
+	}
+}
