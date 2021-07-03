58f14d8f7f4a990e4e5e6bf0f6c546b8c3e0d53e
NightFeather <reingeass@gmail.com>
2021-07-04 00:49:42 +0800

    Create NF03.java

---
+++ Java/demo/NF03.java
@@ -0,0 +1,15 @@
+class Main {
+	public static void main(String args[]) {
+		int x, y;
+		x = 10;
+		y = 20;
+
+		if(x<y) System.out.println("X < Y");
+
+		x = x*2;
+		if(x==y) System.out.println("X = Y");
+
+		x *= 2;
+		if(x>y) System.out.println("X > Y");
+	}
+}
