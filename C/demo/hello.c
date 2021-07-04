20ca2d0056c7eaca6fa52eee74c1b0074a26e27a
NightFeather <reingeass@gmail.com>
2021-07-05 00:02:25 +0800

    Update hello.c

--- C/demo/hello.c
+++ C/demo/hello.c
@@ -2,5 +2,12 @@
 
 int main(void) {
 	printf("%s\n", "hello");
+
+	printf("Hello world\n");
+	
+	char welcome[] = {'W','e','l','c','o','m','e'};
+	printf(welcome);
+	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	return 0;
 }
