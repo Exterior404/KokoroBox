09e5a484046b9a5e86c091d1e5af3002661d9d13
NightFeather <reingeass@gmail.com>
2021-07-13 20:15:34 +0800

    Create NF07.java

---
+++ Java/demo/NF07.java
@@ -0,0 +1,13 @@
+class Main {
+	public static void main(String args[]) {
+		double pi, r, a;
+
+		r = 10.8;
+		pi = 3.1416;
+		a = pi *r *r;
+
+		System.out.println("Area of circle is = " + a);
+	}
+} //366.43622
+
+//repl ^ 不能用
