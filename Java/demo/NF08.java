b709ccdedc81319a7cb723fb29b57ee2fc2e1024
NightFeather <reingeass@gmail.com>
2021-07-14 17:56:03 +0800

    Create NF08.java

---
+++ Java/demo/NF08.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main(String args[]) {
+		char ch1, ch2;
+
+		ch1 = 78; //ASC II字碼表
+		ch2 = 'F';
+
+		System.out.print("cha1 & cha2:");
+		System.out.print("'" + ch1 + ch2 + "'");
+	}
+}
