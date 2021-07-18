2ba8722ec71258edf79c5e163d3df11f43b6f4df
NightFeather <reingeass@gmail.com>
2021-07-18 09:49:37 +0800

    Create NF09.java

---
+++ Java/demo/NF09.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main(String args[]) {
+		char ch1;
+		ch1 = 'X';
+
+		System.out.print("ch1 = " + ch1 + "\n");
+
+		ch1++;
+		System.out.print("ch1++ = " + ch1);
+	}
+}//repl 的java 可以插入C的\n
