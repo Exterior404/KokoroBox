1c35a09427a1d29908af9a593fee02ede968792e
NightFeather <reingeass@gmail.com>
2021-07-19 19:10:30 +0800

    Create NF10.java

---
+++ Java/demo/NF10.java
@@ -0,0 +1,14 @@
+class Main {
+	public static void main(String args[]) {
+		boolean b;
+
+		b = false;
+		System.out.print("b is = " + b);
+		b = true;
+		System.out.print("b is = " + b);
+
+		if(b)System.out.print("True");
+		b = false;
+		if(b)System.out.print("False");
+	}
+}//這裡加個!
