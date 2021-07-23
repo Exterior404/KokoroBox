40ebb309cfb4a384cdaae29e1e0421f485fda899
NightFeather <reingeass@gmail.com>
2021-07-23 18:04:21 +0800

    Create NF13.java

---
+++ Java/demo/NF13.java
@@ -0,0 +1,13 @@
+class Main{
+	public static void main(String args[]) {
+		int x;
+
+		for( x = 0; x < 3; x++) {
+			int  y = -1;
+			System.out.print("\ny is " + y);
+
+			y = 100;
+			System.out.print("\n y is now: " +y);
+		}
+	}
+}
