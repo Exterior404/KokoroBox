9a04ed6bea870eb795aa1f2ec264dc6c2165adc3
NightFeather <reingeass@gmail.com>
2021-07-23 18:04:17 +0800

    Create NF11.java

---
+++ Java/demo/NF11.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String args[]) {
+		double a = 3.0, b = 4.0;
+
+		double c = Math.sqrt(a * a + b * b);
+		// 9 + 16 = 25
+		// 25 的平方根=5
+		System.out.print("Hypotenuse is = " + c);
+	}
+}//Math repl 可以用
