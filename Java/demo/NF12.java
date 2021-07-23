f51687a2e05b904522ecacee9f2c762599ac6405
NightFeather <reingeass@gmail.com>
2021-07-23 18:04:19 +0800

    Create NF12.java

---
+++ Java/demo/NF12.java
@@ -0,0 +1,16 @@
+class Main {
+	public static void main(String args[]) {
+		int x;
+
+		x = 10;
+		if(x == 10){
+			int y = 20;
+		
+
+		System.out.print("X & Y: " + x + " " + y);
+
+		x = y *2;
+		}
+		System.out.print("\nX is " + x);
+	}
+}
