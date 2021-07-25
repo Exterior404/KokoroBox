3593beefdcd97e8ef9a7eecd3149820abe7a5c88
NightFeather <reingeass@gmail.com>
2021-07-25 19:54:02 +0800

    Create NF15.java

---
+++ Java/demo/NF15.java
@@ -0,0 +1,21 @@
+class Main {
+	public static void main(String args[]) {
+		int month_days[];
+    	month_days = new int[12];
+
+    	month_days[0] = 11;
+    	month_days[1] = 12;
+    	month_days[2] = 13;
+    	month_days[3] = 21;
+    	month_days[4] = 22;
+    	month_days[5] = 23;
+    	month_days[6] = 31;
+   		month_days[7] = 32;
+    	month_days[8] = 33;
+    	month_days[9] = 41;
+    	month_days[10] = 42;
+    	month_days[11] = 43;
+
+    	System.out.print("April has " + month_days[3] + " days.");
+	}
+}
