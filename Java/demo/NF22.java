1cff5efdff5f9ff9a87f2154b59048bf7b6da81e
NightFeather <reingeass@gmail.com>
2021-08-18 09:40:08 +0800

    Create NF22.java

---
+++ Java/demo/NF22.java
@@ -0,0 +1,29 @@
+class Main {
+	public static void main(String args[]) {
+		System.out.print("Integer Arithmetic");
+		int a = 1+1;
+		int b = a*3;
+		int c = b/4;
+		int d = c-a;
+		int e = -d;
+
+		System.out.print("a = " + a +"\n");
+		System.out.print("b = " + b +"\n");
+		System.out.print("c = " + c +"\n");
+		System.out.print("d = " + d +"\n");
+		System.out.print("e = " + e +"\n");
+
+
+		double da = 1+1;
+		double db = da*3;
+		double dc = db/4;
+		double dd = dc - da;
+		double de = -dd;
+
+		System.out.print("da = " + da +"\n");
+		System.out.print("db = " + db +"\n");
+		System.out.print("dc = " + dc +"\n");
+		System.out.print("dd = " + dd +"\n");
+		System.out.print("de = " + de +"\n");
+	}
+}
