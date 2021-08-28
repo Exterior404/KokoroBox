0564c09f6cb7d9c60c6c0ade09f052819c544b08
NightFeather <reingeass@gmail.com>
2021-08-28 18:48:48 +0800

    Create NF32.java

---
+++ Java/demo/NF32.java
@@ -0,0 +1,14 @@
+class Main {
+	public static void main(String args[]) {
+		int i, k;
+		i = 10;
+		k = i <0 ? -i:i;
+		System.out.print("Absolute value of \n");
+		System.out.print(i + " is " + k +"\n");
+
+		i = -10;
+		k = i < 0 ? -i:i;
+		System.out.print("Absolute value of \n");
+		System.out.print(i + " is " + k +"\n");
+	}
+}
