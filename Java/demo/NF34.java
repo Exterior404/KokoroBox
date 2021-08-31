bcb0853c57ccd4607b8d2bd344a3d7e68545fa96
NightFeather <reingeass@gmail.com>
2021-08-31 20:36:41 +0800

    Create NF34.java

---
+++ Java/demo/NF34.java
@@ -0,0 +1,22 @@
+class Main {
+	public static void main(String args[]) {
+		for(int i=0; i<6; i++) {
+			switch(i) {
+				case 0:
+					System.out.print("i is zero \n");
+					break;
+				case 1:
+					System.out.print("i is one \n");
+					break;
+				case 2:
+					System.out.print("i is two \n");
+					break;
+				case 3:
+					System.out.print("i is three \n");
+					break;
+				default:
+					System.out.print("i is greater than 3 \n");
+			}
+		}
+	}
+}
